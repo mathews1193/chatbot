904c1ada54f95a1b17fb165b458918c920f15e46
Iyadh <ibouzghi@central.uh.edu>
2022-01-20 22:30:01 -0500

    initial commit

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+}
 }
 
 
